470c3477bad225e2b6966ce503db4b2e08cfbd66
mazvira <mazvira@ukr.net>
2018-02-24 22:02:58 +0200

    Add files via upload

---
+++ StationManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    static class StationManager
+    {
+        public static Person CurrentPerson { get; set; }
+    }
+}
